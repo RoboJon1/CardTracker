9da03e0c0554955609092ca0de9f912d1e5ca381
RoboJon1 <jonlou05@gmail.com>
2023-11-02 13:34:59 -0500

    a

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let sport = []
-let years = [2023, 2019, 2023, 2023, 2023, 2019, 2019];
+var sports = ["football", "football", "football", "football", "baseball", "baseball", "baseball"];
+var years = [2023, 2019, 2023, 2023, 2023, 2019, 2019];
+var players = ["Shedeur Sanders", "Josh Allen", "Shedeur Sanders", "Shedeur Sanders", "Shedeur Sanders", "Shohei Ohtani", "Shohei Ohtani", "Shohei Ohtani"];
+
+function filter(element) {
+    var sport = element.id;
+    var cards = document.getElementById("card-bar").querySelectorAll("card");  
+    console.log(cards);
+    for(let i = 0; i < cards.length; i++) {
+        if(sports[i] == sport) {
+            cards[i].style.display = "flex";
+        }
+        else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
