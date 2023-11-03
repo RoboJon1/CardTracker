fff0585921f07236e8f47cd8b5ab9e9c540565c4
RoboJon1 <jonlou05@gmail.com>
2023-11-03 13:49:00 -0500

    a

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 var sports = ["football", "football", "football", "football", "baseball", "baseball", "baseball"];
+var sportsFilter = [];
 var years = [2023, 2019, 2023, 2023, 2023, 2019, 2019];
+var yearsFilter = [];
 var players = ["Shedeur Sanders", "Josh Allen", "Shedeur Sanders", "Shedeur Sanders", "Shedeur Sanders", "Shohei Ohtani", "Shohei Ohtani", "Shohei Ohtani"];
+var playersFilter = [];
 
 function filter(element) {
-    var sport = element.id;
-    var cards = document.getElementById("card-bar").querySelectorAll("card");  
-    for(let i = 0; i < cards.length; i++) {
-        console.log("hello");
-        if(sports[i] == sport) {
-            cards[i].style.display = "flex";
-        }
-        else {
-            cards[i].style.display = "none";
-        }
+    var checkboxes = document.getElementById(element.parentElement.id).querySelectorAll('input[type=checkbox]:checked');
+    checkboxes.forEach(checkbox => sportsFilter.push(checkbox.id));
+    
+    sportsFilter.push(element.id);
+    var cards = document.getElementById("card-bar").querySelectorAll("div.card"); 
+
+    if(sportsFilter.length > 0) {
+        cards.forEach(card => card.style.display = "none");
     }
+    else {
+        cards.forEach(card => card.style.display = "flex");
+    }
+
+    sportsFilter.forEach(sport => {
+        cards.forEach(function(card, index) {
+            if(sport == sports[index]) {
+                card.style.display = "flex";
+            }
+        })
+    })
+    
+    sportsFilter = [];
 }
 
