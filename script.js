99f656de34ef0852dc4f50ae98bf8f79b1d5416a
RoboJon1 <jonlou05@gmail.com>
2023-11-06 13:49:09 -0600

    a

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var sports = ["football", "football", "football", "football", "baseball", "baseball", "baseball"];
+var sports = ["football", "football", "football", "football", "baseball", "baseball", "baseball", "soccer"];
 var sportsFilter = [];
-var years = [2023, 2019, 2023, 2023, 2023, 2019, 2019];
+var years = [2023, 2019, 2023, 2023, 2023, 2019, 2019, 2022];
 var yearsFilter = [];
-var players = ["Shedeur Sanders", "Josh Allen", "Shedeur Sanders", "Shedeur Sanders", "Shedeur Sanders", "Shohei Ohtani", "Shohei Ohtani", "Shohei Ohtani"];
+var players = ["Shedeur Sanders", "Josh Allen", "Shedeur Sanders", "Shedeur Sanders", "Shedeur Sanders", "Shohei Ohtani", "Shohei Ohtani", "Shohei Ohtani", "Lionel Messi"];
 var playersFilter = [];
 
 function filter(element) {
-    var checkboxes = document.getElementById(element.parentElement.id).querySelectorAll('input[type=checkbox]:checked');
+    var checkboxes = document.getElementById(element.parentElement.parentElement.id).querySelectorAll('input[type=checkbox]:checked');
     checkboxes.forEach(checkbox => sportsFilter.push(checkbox.id));
-    
-    sportsFilter.push(element.id);
+    checkboxes.forEach(checkbox => yearsFilter.push(checkbox.id));
+    checkboxes.forEach(checkbox => playersFilter.push(checkbox.id));
+
     var cards = document.getElementById("card-bar").querySelectorAll("div.card"); 
 
     if(sportsFilter.length > 0) {
@@ -18,7 +19,7 @@ function filter(element) {
     else {
         cards.forEach(card => card.style.display = "flex");
     }
-
+/*
     sportsFilter.forEach(sport => {
         cards.forEach(function(card, index) {
             if(sport == sports[index]) {
@@ -26,7 +27,23 @@ function filter(element) {
             }
         })
     })
-    
-    sportsFilter = [];
+*/
+
+    cards.forEach(function (card, index) {
+        var sportMatch = sportsFilter.length === 0 || sportsFilter.includes(sports[index]);
+        var yearMatch = yearsFilter.length === 0 || yearsFilter.includes(years[index].toString());
+        var playerMatch = playersFilter.length === 0 || playersFilter.includes(players[index]);
+
+        if (sportMatch && yearMatch && playerMatch) {
+            card.style.display = "flex";
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    var sportsFilter = [];
+    var yearsFilter = [];
+    var playersFilter = [];
 }
+
 
