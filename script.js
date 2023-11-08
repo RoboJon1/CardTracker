bf83b5b8b0eb39dc1c828c1770718d251dcaf5d7
RoboJon1 <jonlou05@gmail.com>
2023-11-07 21:33:55 -0600

    a

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var sports = ["football", "football", "football", "football", "baseball", "baseball", "baseball", "soccer"];
 var sportsFilter = [];
-var years = [2023, 2019, 2023, 2023, 2023, 2019, 2019, 2022];
+var years = ["2023", "2019", "2023", "2023", "2023", "2019", "2019", "2022"];
 var yearsFilter = [];
-var players = ["Shedeur Sanders", "Josh Allen", "Shedeur Sanders", "Shedeur Sanders", "Shedeur Sanders", "Shohei Ohtani", "Shohei Ohtani", "Shohei Ohtani", "Lionel Messi"];
+var players = ["Shedeur Sanders", "Josh Allen", "Shedeur Sanders", "Shedeur Sanders", "Shedeur Sanders", "Shohei Ohtani", "Shohei Ohtani", "Lionel Messi"];
 var playersFilter = [];
 
 function filter(element) {
-    var checkboxes = document.getElementById(element.parentElement.parentElement.id).querySelectorAll('input[type=checkbox]:checked');
-    checkboxes.forEach(checkbox => sportsFilter.push(checkbox.id));
-    checkboxes.forEach(checkbox => yearsFilter.push(checkbox.id));
-    checkboxes.forEach(checkbox => playersFilter.push(checkbox.id));
+    var checkboxes = element.parentElement.parentElement.querySelectorAll('input[type=checkbox]:checked');
+    if(element.parentElement.parentElement.id == "sports") checkboxes.forEach(checkbox => sportsFilter.push(checkbox.id));
+    if(element.parentElement.parentElement.id == "years") checkboxes.forEach(checkbox => yearsFilter.push(checkbox.id));
+    if(element.parentElement.parentElement.id == "players") checkboxes.forEach(checkbox => playersFilter.push(checkbox.id));
 
     var cards = document.getElementById("card-bar").querySelectorAll("div.card"); 
 
@@ -19,19 +19,10 @@ function filter(element) {
     else {
         cards.forEach(card => card.style.display = "flex");
     }
-/*
-    sportsFilter.forEach(sport => {
-        cards.forEach(function(card, index) {
-            if(sport == sports[index]) {
-                card.style.display = "flex";
-            }
-        })
-    })
-*/
 
     cards.forEach(function (card, index) {
         var sportMatch = sportsFilter.length === 0 || sportsFilter.includes(sports[index]);
-        var yearMatch = yearsFilter.length === 0 || yearsFilter.includes(years[index].toString());
+        var yearMatch = yearsFilter.length === 0 || yearsFilter.includes(years[index]);
         var playerMatch = playersFilter.length === 0 || playersFilter.includes(players[index]);
 
         if (sportMatch && yearMatch && playerMatch) {
@@ -41,9 +32,16 @@ function filter(element) {
         }
     });
 
-    var sportsFilter = [];
-    var yearsFilter = [];
-    var playersFilter = [];
+    sportsFilter = [];
+    yearsFilter = [];
+    playersFilter = [];
 }
 
+/*
+function createCheckboxes(div) {
+    document.getElementById("sports")
+    document.getElementById("years")
+    document.getElementById("players")
+}
+*/
 
